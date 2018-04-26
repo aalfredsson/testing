a2e3816f80ae8586a2605faedb716b530ae08628
Axel Alfredsson <aalfredsson94@gmail.com>
2018-04-26 11:22:38 +0200

    added yellow background

--- hej.php
+++ hej.php
@@ -5,16 +5,20 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $log = new Logger('Laboration 1');
-$log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));	
-$log->info("Hello,  wlecome here!");
-//var_dump($argv);
-/*
+$log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
+
 $name = $_GET['name'];
-$age = $_GET['age'];
-$email = $_GET['email'];
-echo "";
-echo "name :", $name;
-echo "age: ", $age;
-echo "email: ", $email;
-*/
+$log->info($name);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="background.css">
+<title>Testing, testing</title>
+</head>
+<body>
+<?php
+echo "Hello, " . $name;
+?>
+</body>
+</html>
